4d179f6a6edd273d6102fc6ced20047919bac427
j1sk1ss <fotikolay222@gmail.com>
2025-08-29 11:59:23 +0000

    NASM generation test Pointer logic index support fix

--- src/gen/x86_64_nasm_gnu/x86_64_storegen.c
+++ src/gen/x86_64_nasm_gnu/x86_64_storegen.c
@@ -4,22 +4,22 @@ int x86_64_generate_store(ast_node_t* node, FILE* output, gen_ctx_t* ctx) {
     if (!node->token) return 0;
     if (VRS_isptr(node->token)) {
         if (node->child) goto indexing;
-        else iprintf(output, "mov qword ptr %s, rax\n", GET_ASMVAR(node));
+        else iprintf(output, "mov qword %s, rax\n", GET_ASMVAR(node));
         return 1;
     }
 
     switch (node->token->t_type) {
         case LONG_VARIABLE_TOKEN: 
-            iprintf(output, "mov qword ptr %s, rax\n", GET_ASMVAR(node));
+            iprintf(output, "mov qword %s, rax\n", GET_ASMVAR(node));
         break;
         case INT_VARIABLE_TOKEN:  
-            iprintf(output, "mov dword ptr %s, eax\n", GET_ASMVAR(node));
+            iprintf(output, "mov dword %s, eax\n", GET_ASMVAR(node));
         break;
         case SHORT_VARIABLE_TOKEN:
-            iprintf(output, "mov word ptr %s, ax\n", GET_ASMVAR(node));
+            iprintf(output, "mov word %s, ax\n", GET_ASMVAR(node));
         break;
         case CHAR_VARIABLE_TOKEN: 
-            iprintf(output, "mov byte ptr %s, al\n", GET_ASMVAR(node));
+            iprintf(output, "mov byte %s, al\n", GET_ASMVAR(node));
         break;
         case ARR_VARIABLE_TOKEN:
         case STR_VARIABLE_TOKEN: {
@@ -36,15 +36,16 @@ indexing:
                 if (elsize > 1) {
                     iprintf(output, "imul rax, %d\n", elsize);
                 }
-                
-                iprintf(output, "lea rbx, %s\n", GET_ASMVAR(node));
+
+                if (!node->token->vinfo.ptr) iprintf(output, "lea rbx, %s\n", GET_ASMVAR(node));
+                else iprintf(output, "mov rbx, %s\n", GET_ASMVAR(node));
                 iprintf(output, "add rax, rbx\n");
 
                 switch (elsize) {
-                    case 1: iprintf(output, "mov byte ptr [rax], rdx\n");  break;
-                    case 2: iprintf(output, "mov word ptr [rax], dx\n");   break;
-                    case 4: iprintf(output, "mov dword ptr [rax], rdx\n"); break;
-                    case 8: iprintf(output, "mov qword ptr [rax], rdx\n"); break;
+                    case 1: iprintf(output, "mov byte [rax], rdx\n");  break;
+                    case 2: iprintf(output, "mov word [rax], dx\n");   break;
+                    case 4: iprintf(output, "mov dword [rax], rdx\n"); break;
+                    case 8: iprintf(output, "mov qword [rax], rdx\n"); break;
                 }
             }
             else { /* Loading array address to rax */
