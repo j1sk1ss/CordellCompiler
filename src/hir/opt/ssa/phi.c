d33f386d53b21b8e598d2df930b15e169470d8a2
nikolaj <fotikolay222@gmail.com>
2025-09-25 16:37:16 +0500

    PHI functions

--- src/hir/opt/ssa/phi.c
+++ src/hir/opt/ssa/phi.c
@@ -3,18 +3,18 @@
 static int _has_phi(cfg_block_t* b, long v_id) {
     hir_block_t* h = b->entry;
     while (h) {
-        if (h->op == HIR_PHI && h->farg 
-            && h->farg->storage.var.v_id == v_id) {
+        if (h->op == HIR_PHI && h->farg && h->farg->storage.var.v_id == v_id) {
             return 1;
         }
         if (h == b->exit) break;
         h = h->next;
     }
-
     return 0;
 }
 
 static int _insert_phi_instr(cfg_block_t* b, variable_info_t* vi) {
+    if (_has_phi(b, vi->v_id)) return 0;
+
     token_t tmptkn = { .t_type = vi->type };
     hir_block_t* phi = HIR_create_block(
         HIR_PHI, HIR_SUBJ_STKVAR(vi->v_id, HIR_get_tmptype_tkn(&tmptkn), vi->s_id), NULL, NULL
@@ -30,19 +30,19 @@ int HIR_SSA_insert_phi(cfg_ctx_t* cctx, sym_table_t* smt) {
     for (cfg_func_t* f = cctx->h; f; f = f->next) {
         HIR_CFG_compute_dom(f);
         HIR_CFG_compute_idom(f);
-    }
-
-    for (cfg_func_t* f = cctx->h; f; f = f->next) {
         HIR_CFG_compute_domf(f);
     }
 
-    variable_info_t* vh = smt->v.h;
-    while (vh) {
-        if (!vh->glob) {
-            set_t defs;
-            set_init(&defs);
-            HIR_CFG_collect_defs(vh->v_id, cctx, &defs);
+    for (variable_info_t* vh = smt->v.h; vh; vh = vh->next) {
+        if (vh->glob) continue;
 
+        set_t defs;
+        set_init(&defs);
+        HIR_CFG_collect_defs(vh->v_id, cctx, &defs);
+
+        int changed;
+        do {
+            changed = 0;
             set_iter_t it;
             set_iter_init(&defs, &it);
 
@@ -55,15 +55,16 @@ int HIR_SSA_insert_phi(cfg_ctx_t* cctx, sym_table_t* smt) {
                 while ((f = (cfg_block_t*)set_iter_next_addr(&fit))) {
                     if (!_has_phi(f, vh->v_id)) {
                         _insert_phi_instr(f, vh);
-                        if (!set_has_addr(&defs, f)) set_add_addr(&defs, f);
+                        if (!set_has_addr(&defs, f)) {
+                            set_add_addr(&defs, f);
+                            changed = 1;
+                        }
                     }
                 }
             }
+        } while (changed);
 
-            set_free(&defs);
-        }
-
-        vh = vh->next;
+        set_free(&defs);
     }
 
     return 1;
