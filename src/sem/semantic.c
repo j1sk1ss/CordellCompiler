2626094e90f1ca0aec3f79c13b7804e4b3f0fb6e
nikolaj <fotikolay222@gmail.com>
2025-11-26 19:03:34 +0500

    [Code - Semantic] Semantic updates implementation

--- src/sem/semantic.c
+++ src/sem/semantic.c
@@ -1,11 +1,14 @@
 #include <sem/semantic.h>
 
-int SEM_perform_ast_check(ast_ctx_t* actx) {
+int SEM_perform_ast_check(ast_ctx_t* actx, sym_table_t* smt) {
     ast_walker_t walker;
-    ASTWLK_init_ctx(&walker);
+    ASTWLK_init_ctx(&walker, smt);
 
     ASTWLK_register_visitor(ASSIGN_NODE, ASTWLKR_ro_assign, &walker);
-    ASTWLK_register_visitor(ASSIGN_NODE, ASTWLKR_rtype_assign, &walker);
+    ASTWLK_register_visitor(DECLARATION_NODE | ASSIGN_NODE | EXPRESSION_NODE, ASTWLKR_rtype_assign, &walker);
+    ASTWLK_register_visitor(DECLARATION_NODE, ASTWLKR_not_init, &walker);
+    ASTWLK_register_visitor(FUNCTION_NODE, ASTWLKR_no_return, &walker);
+    ASTWLK_register_visitor(START_NODE, ASTWLKR_no_exit, &walker);
 
     ASTWLK_walk(actx, &walker);
     ASTWLK_unload_ctx(&walker);
