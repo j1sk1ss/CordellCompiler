92cb577bcbac786a8e75cdd1bf355e2ab1b68e3b
nikolaj <fotikolay222@gmail.com>
2025-11-26 19:03:16 +0500

    [Code - Semantic] Semantic updates

--- include/sem/semantic.h
+++ include/sem/semantic.h
@@ -2,9 +2,12 @@
 #define SEMANTIC_H_
 
 #include <std/list.h>
+#include <symtab/symtab.h>
 #include <ast/ast.h>
 #include <ast/astgen.h>
 #include <sem/ast/ast_walker.h>
 #include <sem/ast/ast_visitors.h>
+
+int SEM_perform_ast_check(ast_ctx_t* actx, sym_table_t* smt);
 
 #endif
