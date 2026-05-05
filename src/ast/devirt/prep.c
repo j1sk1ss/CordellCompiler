dd6b72258e80317b47a971ce2f97d0079633924c
nikolaj <fotikolay222@gmail.com>
2026-05-05 23:32:20 +0500

    [AST] Pointer calls for generic functions

--- src/ast/devirt/prep.c
+++ src/ast/devirt/prep.c
@@ -31,14 +31,20 @@ static int _find_and_register_resolved_call(ast_node_t* node, sym_table_t* smt, 
     _find_and_register_resolved_call(node->siblings.n, smt, ctx);
     _find_and_register_resolved_call(node->c, smt, ctx);
     if (!node->t) return 0;
+
+    ast_node_t* name = NULL;
     if (
-        node->t->t_type == CALLING_TOKEN      &&
-        node->c->t->t_type == FUNC_NAME_TOKEN &&
-        node->c->c
-    ) {
+        node->t->t_type == CALLING_TOKEN &&
+        node->c->t->t_type == FUNC_NAME_TOKEN
+    ) name = node->c;
+    else if (
+        node->t->t_type == CALL_ADDR_TOKEN
+    ) name = node;
+
+    if (name && name->c) {
         list_t types;
         list_init(&types);
-        ast_node_t* type_node = node->c->c;
+        ast_node_t* type_node = name->c;
         while (type_node) {
             if (type_node->t->t_type == GENERIC_TYPE_TOKEN) {
                 list_free(&types);
@@ -49,16 +55,16 @@ static int _find_and_register_resolved_call(ast_node_t* node, sym_table_t* smt, 
             type_node = type_node->siblings.n;
         }
         
-        string_t* section = SCTB_get_section_name(node->c->sinfo.v_id, SECTION_ELEMENT_FUNCTION, &smt->c);
+        string_t* section = SCTB_get_section_name(name->sinfo.v_id, SECTION_ELEMENT_FUNCTION, &smt->c);
         if (!section) section = create_string(CONF_get_code_section());
         else section = section->copy(section);
         
-        symbol_id_t base = node->c->sinfo.v_id;
-        node->c->sinfo.v_id = FNTB_create_resolved_copy(base, &types, &smt->f);
-        node->c->sinfo.s_id = NO_SYMBOL_ID;
-        AST_DVRT_register_implementation(node->c->sinfo.v_id, base, ctx);
+        symbol_id_t base = name->sinfo.v_id;
+        name->sinfo.v_id = FNTB_create_resolved_copy(base, &types, &smt->f);
+        name->sinfo.s_id = NO_SYMBOL_ID;
+        AST_DVRT_register_implementation(name->sinfo.v_id, base, ctx);
         
-        SCTB_add_to_section(section, node->c->sinfo.v_id, SECTION_ELEMENT_FUNCTION, &smt->c);
+        SCTB_add_to_section(section, name->sinfo.v_id, SECTION_ELEMENT_FUNCTION, &smt->c);
         destroy_string(section);
         list_free(&types);
     }
