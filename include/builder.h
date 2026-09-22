4018ee77cc75b41cbe898be7ebc04b10c85ac731
j1sk1ss <fotikolay222@gmail.com>
2026-09-22 09:46:50 -0700

    [Minor] Version update

--- include/builder.h
+++ include/builder.h
@@ -84,8 +84,8 @@
 #include <symtab/dump.h>
 
 #include <gem_data.h>
-#define CCPL_VERSION                 "3.8.2:2009.26" // major.minor<.patch> (old version style):ddmm.yy (new version style)
-#define CCPL_SPLASH                  "test branch: interfaces" // NULL / Related to the version splash
+#define CCPL_VERSION                 "3.8.3:2209.26" // major.minor<.patch> (old version style):ddmm.yy (new version style)
+#define CCPL_SPLASH                  "Interfaces and abstractions!" // NULL / Related to the version splash
 /* Version logic is next: We have the old style and the new style:
     - Old style is a default version semantics - major-minor-patch style, where major is incremented when
       I've added a lot of new features and they work properly. Also there should be some big shifts in
